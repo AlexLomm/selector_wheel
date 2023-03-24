34ab6f26a99dffb52ad16ff28a64d6675680879d
Alex Lomia <alex.lomia@gmail.com>
2023-03-24 04:10:48 -0400

    Improve examples in example/main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,11 @@ class MainApp extends StatelessWidget {
                 child: SelectorWheel(
                   childCount: 10,
                   convertIndexToValue: (int index) {
-                    return SelectorWheelValue(label: index.toString(), value: index, index: index);
+                    return SelectorWheelValue(
+                      label: index.toString(),
+                      value: index,
+                      index: index,
+                    );
                   },
                   onValueChanged: (SelectorWheelValue<int> value) {
                     // ignore: avoid_print
@@ -48,18 +52,20 @@ class MainApp extends StatelessWidget {
                   colorScheme: Theme.of(context).colorScheme.copyWith(
                         surface: Colors.black,
                         onSurface: Colors.white,
-                        secondaryContainer: Colors.amber,
+                        secondaryContainer: Colors.purple,
                       ),
                 ),
                 child: _Background(
                   child: SelectorWheel(
-                    highlightBorderRadius: 16.0,
                     childCount: 10,
-                    highlightHeight: 40.0,
-                    highlightWidth: 100.0,
+                    childHeight: 48.0,
+                    highlightBorderRadius: 32.0,
+                    highlightHeight: 48.0,
+                    highlightWidth: 120.0,
+                    fadeOutHeightFraction: 0.33,
                     convertIndexToValue: (int index) {
                       final fraction = index / 4;
-                      final value = fraction.toStringAsFixed(2);
+                      final value = '${fraction.toStringAsFixed(2)} lb';
 
                       return SelectorWheelValue(label: value, value: fraction, index: index);
                     },
@@ -68,6 +74,82 @@ class MainApp extends StatelessWidget {
                       print(value);
                     },
                   ),
+                ),
+              ),
+              const SizedBox(height: 32.0),
+              // ----------------------------------------------
+              // Example 3, customized wheel selectors displayed side by side
+              // ----------------------------------------------
+              _Background(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Theme(
+                      data: Theme.of(context).copyWith(
+                        colorScheme: Theme.of(context).colorScheme.copyWith(
+                              secondaryContainer: Colors.amber,
+                            ),
+                      ),
+                      child: SelectorWheel(
+                        width: 60.0,
+                        childCount: 10,
+                        highlightBorderRadius: 4,
+                        convertIndexToValue: (int index) {
+                          final value = index * 5;
+
+                          return SelectorWheelValue(
+                            label: value.toString(),
+                            value: value,
+                            index: index,
+                          );
+                        },
+                        onValueChanged: (SelectorWheelValue<int> value) {
+                          // ignore: avoid_print
+                          print(value);
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 32.0,
+                      child: Text(
+                        '✕',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    Theme(
+                      data: Theme.of(context).copyWith(
+                        textTheme: Theme.of(context).textTheme.copyWith(
+                              titleLarge: Theme.of(context).textTheme.titleLarge?.copyWith(
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                        colorScheme: Theme.of(context).colorScheme.copyWith(
+                              // surface: Colors.black,
+                              // onSurface: Colors.white,
+                              secondaryContainer: Colors.lime,
+                            ),
+                      ),
+                      child: SelectorWheel(
+                        width: 60.0,
+                        childCount: 4,
+                        highlightBorderRadius: 4,
+                        convertIndexToValue: (int index) {
+                          const fruits = ['🍇', '🍍', '🍒', '🥥', '🍓', '🥭', '🍈'];
+
+                          final value = fruits[index];
+
+                          return SelectorWheelValue(label: value, value: value, index: index);
+                        },
+                        onValueChanged: (SelectorWheelValue<String> value) {
+                          // ignore: avoid_print
+                          print(value);
+                        },
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
@@ -89,7 +171,7 @@ class _Background extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 180.0,
+      width: 300.0,
       height: 180.0,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16.0),
